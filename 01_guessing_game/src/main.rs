34ab70d98b17ecfa6f3e90a87805919eb218304f
Minh Ha <mihado@minh.im>
2019-05-28 11:32:50 +1200

    use rand & std::cmp

--- 01_guessing_game/src/main.rs
+++ 01_guessing_game/src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number:");
+
+    let secret = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret is {}", secret);
 
     println!("Please input your guess.");
 
@@ -10,5 +16,14 @@ fn main() {
     io::stdin().read_line(&mut guess)
                .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse()
+                          .expect("Please input a number");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret) {
+        Ordering::Less    => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal   => println!("YAY!"),
+    }
 }
